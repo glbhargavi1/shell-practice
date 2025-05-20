80efbe3afa96346718ca14c5268d4fc019b1783e
lakshmi <glbhargavi1@gmail.com git config --global user.email glbhargavi1@gmail.com>
2025-05-20 11:36:59 +0530

    quote

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+$person1 = modi
+$person2 = musk
+
+echo "$person1:: Hey $person2, How are you?"
+echo "$person2:: Hello $person1, I am fine. How are you doing"
+echo "$person1:: Iam fine too. What's up?"
+echo "$person2 :: Nothing,just going to mars now ,will you come"
+echo "$person1:: Sorry, you carry on ! I will come once u come back"
