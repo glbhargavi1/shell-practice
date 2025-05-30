e081149015285ea80919183b59cd2af34f9918eb
lakshmi bhargavi <glbhargavi1@gmail.com>
2025-05-30 12:24:13 +0100

    args

---
+++ 04-variables.sh
@@ -0,0 +1,10 @@
+
+person1=$1 
+person2=$2
+
+
+echo "$person1: Hey $person2 , How are you?"
+echo "$person2  : Hello Trump, I am fine. How are you doing"
+echo "$person1: I am fine too. What's up?"
+echo "$person2  : Nothing, just going to Mars now, will you come"
+echo "$person1: Sorry, you carry on! I will come once you come back"
