adf8cd3f25fd0b414f45b6e7e44d75af9fd57bb1
lakshmi bhargavi <glbhargavi1@gmail.com>
2025-05-30 06:25:27 +0100

    talked

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+person1=Trump
+person2=Musk
+
+
+echo "$person1: Hey $person2 , How are you?"
+echo "$person2  : Hello Trump, I am fine. How are you doing"
+echo "$person1: I am fine too. What's up?"
+echo "$person2  : Nothing, just going to Mars now, will you come"
+echo "$person1: Sorry, you carry on! I will come once you come back"
