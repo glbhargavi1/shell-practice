512cd035f5af5ca1076d50cbc214af016bfe05e6
lakshmi bhargavi <glbhargavi1@gmail.com>
2025-05-31 13:32:01 +0100

    datatypes

--- 04-variables.sh
+++ 04-variables.sh
@@ -4,7 +4,7 @@ person2=$2
 
 
 echo "$person1: Hey $person2 , How are you?"
-echo "$person2  : Hello Trump, I am fine. How are you doing"
+echo "$person2  : Hello $person1, I am fine. How are you doing"
 echo "$person1: I am fine too. What's up?"
 echo "$person2  : Nothing, just going to Mars now, will you come"
 echo "$person1: Sorry, you carry on! I will come once you come back"
