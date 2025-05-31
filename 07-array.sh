f67bb7a156c983f7d3ab54749b35943a2b009fa0
lakshmi bhargavi <glbhargavi1@gmail.com>
2025-05-31 05:01:25 +0100

    movies

--- 07-array.sh
+++ 07-array.sh
@@ -7,4 +7,6 @@ MOVIES=("court" "HIT3" "pushpa2" "thandel")
 echo "First Movie : ${MOVIES[0]}"
 echo "second Movie: ${MOVIES[1]}"
 
-echo "All Movies : ${MOVIES[%d]}"
+echo "All Movies : ${MOVIES[@]}"
+
+echo "Fifth Movie : ${MOVIES[4]}"
