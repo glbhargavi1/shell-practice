7b4a7271673208f2d5034b08c7642128e3696b4d
lakshmi bhargavi <glbhargavi1@gmail.com>
2025-06-03 11:48:50 +0530

    spclvars

--- 08-special-vars.sh
+++ 08-special-vars.sh
@@ -2,3 +2,10 @@
 
 echo "All variables passed to the script: $@"
 echo "Number of variables: $#"
+echo "script name: $0"
+echo "Current Directory: $PWD"
+echo "User running the script: $USER"
+echo "Home directory of user: $HOME"
+echo "PID of the script: $$"
+sleep 10 &
+echo "PID of last command in the background: $!"
